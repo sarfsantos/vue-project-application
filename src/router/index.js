c4c2a665e6da9e563ee4be66581e8175f5947ed2
Sandra Santos <sarfsantos.ss@gmail.com>
2025-06-06 12:12:16 +0100

    update index

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Homepage from '@/views/OnePage.vue'
-import About from '@/views/About.vue'
+import About from '@/views/PageAbout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
