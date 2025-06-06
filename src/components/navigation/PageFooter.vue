749cca2484788d4013ec78aceb57a227332ab071
Sandra Santos <sarfsantos.ss@gmail.com>
2025-06-06 23:06:02 +0100

    change cols

--- src/components/navigation/PageFooter.vue
+++ src/components/navigation/PageFooter.vue
@@ -2,17 +2,17 @@
   <section class="footer-section">
     <div class="container">
       <div class="row">
-        <div class="col-lg-12">
+        <div class="col-12">
           <ul class="menu-list-footer">
             <li><a class="menu-item button-1" href="#benefits">Benefits</a></li>
             <li><a class="menu-item button-1" href="#specifications">Specifications</a></li>
             <li><a class="menu-item button-1" href="#how-to">How-to</a></li>
           </ul>
         </div>
-        <div class="col-lg-6 text-left">
+        <div class="col-6 text-left">
           <p class="label-2">© Area. 2025</p>
         </div>
-        <div class="col-lg-6 text-right">
+        <div class="col-6 text-right">
           <p class="label-2">All Rights Reserved</p>
         </div>
       </div>
